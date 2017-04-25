007c17555dfe679e892038da93b6380e3b556af2
adan <adank92@gmail.com>
2017-04-24 23:06:38 -0300

    Incomprehensibly Concise

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,40 +1,19 @@
 class Bottles
-  def verse(initial)
-    return zero_verse if initial == 0
-    left = initial - 1
-    <<-VERSE
-#{bottles_string(initial)} of beer on the wall, #{bottles_string(initial)} of beer.
-Take #{down_string(left)} down and pass it around, #{bottles_string(left)} of beer on the wall.
-VERSE
-  end
-
-  def verses(from, to)
-    range = to..from
-    range.to_a.reverse.map { |initial| verse(initial) }.join("\n")
-  end
-
   def song
-    verses 99, 0
+    verses(99, 0)
   end
 
-  private
-
-  def zero_verse
-    <<-VERSE
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-VERSE
+  def verses(hi, lo)
+    hi.downto(lo).map { |n| verse(n) }.join("\n")
   end
 
-  def bottles_string(amount)
-    return 'no more bottles' if amount == 0
-    string = "#{amount} bottle"
-    string += 's' if amount > 1
-    string
-  end
-
-  def down_string(amount)
-    return 'it' if amount == 0
-    "one"
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+    " of beer on the wall, " +
+    "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+    "#{n > 0  ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+              : "Go to the store and buy some more"}, " +
+    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+    " of beer on the wall.\n"
   end
 end
