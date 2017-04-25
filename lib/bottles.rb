9528c73ef06b52a34da243e1fcf848b2ccc96ee8
adan <adank92@gmail.com>
2017-04-25 20:38:04 -0300

    Added line-break

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,6 +3,6 @@ class Bottles
     "99 bottles of beer on the wall, " +
     "99 bottles of beer.\n" +
     "Take one down and pass it around, " +
-    "98 bottles of beer on the wall"
+    "98 bottles of beer on the wall.\n"
   end
 end
